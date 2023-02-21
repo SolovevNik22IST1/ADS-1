91236815c9bf4095a3057b0d144aac481f382a1f
SolovevNik22IST1 <125061447+SolovevNik22IST1@users.noreply.github.com>
2023-02-21 18:33:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,60 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (n > 1)
+    {
+      for (int i = 2; i < n; i++)
+          if (n % i == 0)
+              return false;
+          return true;
+    }
+    else
+        return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int  j, a, N;
+ 
+    printf("Input N: ");
+    scanf("%d", &N);
+ 
+    a = 0;
+    for (int p = 2;; p++)  {
+        for (j = 2; j*j <= p; j++)    {
+            if ((p % j) == 0)   break;
+            if (j*j > p)   {
+                a++;
+                if (a == N)    {
+                    printf("%d", p);
+                    break;
+                }
+            }
+        }
+    }
+    system("Pause");
+    return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+int value;
+  int a;
+  printf("Input value");
+  scanf("%d", &value);
+  while(True){
+    for (int i = value; i <value*value; i++){
+          if (value % i == 0)
+            break;
+      a=i;
+    }
+    printf("%d", a);
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int end, sum;
+  printf("Input end number");
+  scanf("%d", &end);
+  for (int i = 0; i < end; i++){
+    sum+=i;
+  }
+  printf("%d", sum);
 }
